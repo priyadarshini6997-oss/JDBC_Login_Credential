90574cf511652a21afbbf228f9590213fa7a14f2
Priyadarshini C <priyadarshini6997@gmail.com>
2026-02-06 22:08:15 +0530

    Add files via upload

---
+++ SQL_Ass16_1.sql
@@ -0,0 +1,15 @@
+create database DBemp;
+use DBemp;
+create table employee(
+id int primary key ,
+username varchar(50),
+password varchar(50)
+);
+insert into employee(id,username,password) values
+('1','admin','admin123'),
+('2','priya','priya123'),
+('3','john','john123');
+
+
+
+
